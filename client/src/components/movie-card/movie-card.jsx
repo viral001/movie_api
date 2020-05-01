0a861dffb8060ba6f76c5f2e4a69cc0f57fa8e93
viral001 <viral_1996@yahoo.com>
2020-05-01 12:34:47 -0400

    Update movie-card.jsx

--- client/src/components/movie-card/movie-card.jsx
+++ client/src/components/movie-card/movie-card.jsx
@@ -1,32 +1,32 @@
-mport React from 'react';
-import PropTypes from 'prop-types';
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
+import React from "react";
+import PropTypes from "prop-types";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
 
 export class MovieCard extends React.Component {
-    render() {
-        const { movie, onClick } = this.props;
+  render() {
+    const { movie, onClick } = this.props;
 
-        return (
-            <Card style={{ width: '16rem' }}>
-                <Card.Img variant="top" src={movie.ImagePath} />
-                <Card.Body>
-                    <Card.Title>{movie.Title}</Card.Title>
-                    <Card.Text>{movie.Description}</Card.Text>
-                    <Button onClick={() => onClick(movie)} variant="link">
-                        Open
+    return (
+      <Card style={{ width: "16rem" }}>
+        <Card.Img variant="top" src={movie.ImagePath} />
+        <Card.Body>
+          <Card.Title>{movie.Title}</Card.Title>
+          <Card.Text>{movie.Description}</Card.Text>
+          <Button onClick={() => onClick(movie)} variant="link">
+            Open
           </Button>
-                </Card.Body>
-            </Card>
-        );
-    }
+        </Card.Body>
+      </Card>
+    );
+  }
 }
 
 MovieCard.propTypes = {
-    movie: PropTypes.shape({
-        Title: PropTypes.string.isRequired,
-        Description: PropTypes.string.isRequired,
-        ImagePath: PropTypes.string.isRequired,
-    }).isRequired,
-    onClick: PropTypes.func.isRequired,
+  movie: PropTypes.shape({
+    Title: PropTypes.string.isRequired,
+    Description: PropTypes.string.isRequired,
+    ImagePath: PropTypes.string.isRequired,
+  }).isRequired,
+  onClick: PropTypes.func.isRequired,
 };
