6dfbd64bed0d0fb3aa1ddbaf3f61d65a1deed2a5
viral001 <viral_1996@yahoo.com>
2020-06-17 12:18:30 -0400

    updated

--- dist/src.78399e21.js
+++ dist/src.78399e21.js
@@ -39212,7 +39212,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51720" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50038" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
