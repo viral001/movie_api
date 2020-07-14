6d8030ccc13c0464c80376d334503de048beced5
viral001 <viral_1996@yahoo.com>
2020-07-14 13:03:51 -0400

    var
    changed from var to let in models.js

--- models.js
+++ models.js
@@ -1,40 +1,41 @@
-const mongoose = require('mongoose'),
-bcrypt = require('bcrypt');
+const mongoose = require("mongoose"),
+  bcrypt = require("bcrypt");
 
-var movieSchema = mongoose.Schema({
-  Title : {type: String, required: true},
-  Description : {type: String, required: true},
-  Genre : {
-    Name : String,
-    Description : String
+let movieSchema = mongoose.Schema({
+  Title: { type: String, required: true },
+  Description: { type: String, required: true },
+  Genre: {
+    Name: String,
+    Description: String,
   },
-  Director : {
-    Name : String,
-    Bio : String,
-    Birth : Date,
-    Death : Date
+  Director: {
+    Name: String,
+    Bio: String,
+    Birth: Date,
+    Death: Date,
   },
-  ImagePath : String,
-  Featured : Boolean
+  ImagePath: String,
+  Featured: Boolean,
 });
 
-var userSchema = mongoose.Schema({
-  Username : {type: String, required: true},
-  Password : {type: String, required: true},
-  Email : {type: String, required: true},
-  Birthday : Date,
-  FavoriteMovies : [{ type : mongoose.Schema.Types.ObjectId, ref: 'Movie'}]
+let userSchema = mongoose.Schema({
+  Username: { type: String, required: true },
+  Password: { type: String, required: true },
+  Email: { type: String, required: true },
+  Birthday: Date,
+  FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: "Movie" }],
 });
 
-userSchema.statics.hashPassword = function(password) {
+userSchema.statics.hashPassword = function (password) {
   return bcrypt.hashSync(password, 10);
-}
+};
 
-userSchema.methods.validatePassword = function(password) {
-  return bcrypt.compareSync(password, this.Password)};
+userSchema.methods.validatePassword = function (password) {
+  return bcrypt.compareSync(password, this.Password);
+};
 
-var Movie = mongoose.model('Movie', movieSchema);
-var User = mongoose.model('User', userSchema);
+let Movie = mongoose.model("Movie", movieSchema);
+let User = mongoose.model("User", userSchema);
 
 module.exports.Movie = Movie;
 module.exports.User = User;
